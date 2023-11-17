b078dc3cfc840b5bda210f46d110e342e76ee689
Toury Marwane <tmarwane2000@gmail.com>
2023-11-17 14:08:45 +0100

    Premier commit

---
+++ src/main/java/com/iwa/candidats/CandidatsApplication.java
@@ -0,0 +1,13 @@
+package com.iwa.candidats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidatsApplication.class, args);
+	}
+
+}
